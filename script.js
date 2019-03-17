a6e09541b9a806009bc6165d6e0a7d9295f71434
BatHelix <bathelix@gmail.com>
2019-03-16 21:19:21 -0400

    expertimental code

--- script.js
+++ script.js
@@ -1,18 +1,29 @@
 
-var todos = [];
+// var todos = [];
 
-var input = prompt("What would you like to do?");
+// var input = prompt("What would you like to do?");
 
 
 
-while (input !== "quit") {
-if (input === "list") {
-    console.log(todos);
-}   else if (input === "new") {
-    var newTodo = prompt("Enter new todo");
-    todos.push(newTodo);
-}
-//ask for new input
- input = prompt("What would you like to do?");
-}
-console.log("Ok, you quit the app.")
+// while (input !== "quit") {
+// if (input === "list") {
+//     console.log(todos);
+// }   else if (input === "new") {
+//     var newTodo = prompt("Enter new todo");
+//     todos.push(newTodo);
+// }
+// //ask for new input
+//  input = prompt("What would you like to do?");
+// }
+// console.log("Ok, you quit the app.")
+
+
+var colors = ['red','blue','green','yellow'];
+
+colors.forEach(function(dude) {
+    console.log("some color " + dude);
+});
+
+// for (var i=0; i< colors.length; i++) {
+//     console.log(colors[i]);
+// }
