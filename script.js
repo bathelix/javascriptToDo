2c8360861b088cfa29468574be0121e07da56a78
BatHelix <bathelix@gmail.com>
2019-03-16 22:17:13 -0400

    list functions

--- script.js
+++ script.js
@@ -1,29 +1,33 @@
 
-// var todos = [];
+var todos = [];
 
-// var input = prompt("What would you like to do?");
-
-
-
-// while (input !== "quit") {
-// if (input === "list") {
-//     console.log(todos);
-// }   else if (input === "new") {
-//     var newTodo = prompt("Enter new todo");
-//     todos.push(newTodo);
-// }
-// //ask for new input
-//  input = prompt("What would you like to do?");
-// }
-// console.log("Ok, you quit the app.")
+var input = prompt("What would you like to do?");
 
 
-var colors = ['red','blue','green','yellow'];
 
-colors.forEach(function(dude) {
-    console.log("some color " + dude);
-});
+while (input !== "quit") {
 
-// for (var i=0; i< colors.length; i++) {
-//     console.log(colors[i]);
-// }
+    if (input === "list") {
+    console.log("**********************");
+    todos.forEach(function(todo, i) {
+    console.log(i + ": " + todo);
+});   
+    console.log("**********************");
+
+} else if (input === "new") {
+    var newTodo = prompt("Enter new todo");
+    todos.push(newTodo);
+}
+
+ input = prompt("What would you like to do?");
+}
+
+console.log("Ok, you quit the app.")
+
+
+
+
+// todos.forEach(function(exp) {
+//     console.log("some color " + exp);
+// });
+
